180cc89d7e1bfc6c4ad388ae49f7d32b6fd0925a
aljer80 <jernberg.alexandra@gmail.com>
2023-05-02 15:31:32 +0200

    attempt2

--- app/scripts/main.script.js
+++ app/scripts/main.script.js
@@ -1,44 +1,27 @@
-function fetchWcProducts() {
-  const consumerKey = "";
-  const consumerSecret = "";
-  const url =
-    "http://localhost:8888/the_silk_road_project/wp-json/wc/v3/products";
 
-  const requestOptions = {
-    method: "GET",
-    headers: new Headers({
-      "Content-Type": "application/json",
-      Authorization: "Basic " + btoa(consumerKey + ":" + consumerSecret),
-    }),
-  };
+  var orderData = JSON.parse(localStorage.getItem('orders'));
 
-
-  fetch(url, requestOptions)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error));
-  
-  // var myHeaders = new Headers();
-  // myHeaders.append("", "");
-  // myHeaders.append("Access-Control-Request-Method", "");
-  
-  // var requestOptions = {
-  //   method: 'GET',
-  //   headers: myHeaders,
-  //   redirect: 'follow'
-  // };
-  
-  // fetch("http://localhost:8888/the_silk_road_project/wp-json/wc/v3/products?oauth_consumer_key=ck_677a936c591ca7de5803c4cd90953962db1d5e3b&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1683027568&oauth_nonce=fzf5naTTsRU&oauth_version=1.0&oauth_signature=5DVkz%2FEg1lGTdEB5d15T5TlZkKc%3D", requestOptions)
-  //   .then(response => response.text())
-  //   .then(result => console.log(result))
-  //   .catch(error => console.log('error', error));
-
+if(localStorage.getItem('orders')){
+  console.log(orderData);
+  displayOrders(orderData)
+} else {
+  fetchData();
 }
 
-fetchWcProducts();
 
-//från php-koden
-// header( 'Access-Control-Allow-Origin: *' );
-//   header( 'Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS' );
-//   header( 'Access-Control-Allow-Credentials: true' );
-//   header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
+function fetchData(){  
+  var requestOptions = {
+          method: 'GET',
+          redirect: 'follow'
+        };
+  fetch("http://localhost:8888/the_silk_road_project/wp-json/wc/v3/products?oauth_consumer_key=ck_677a936c591ca7de5803c4cd90953962db1d5e3b&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1683033891&oauth_nonce=J3zzNiBS4xT&oauth_version=1.0&oauth_signature=LhUsdvMgBEtCjdfbPJIbfABRBas%3D", requestOptions)
+          .then(response => response.json())
+          .then(result => { orderData = result;
+          console.log(orderData);
+          localStorage.setItem('orders', JSON.stringify(orderData))} )
+          .catch(error => console.log('error', error));
+}
+
+
+
+fetchData();
