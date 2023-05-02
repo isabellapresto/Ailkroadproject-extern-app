02b9ff66c6d1f7607f8b5f1ee6e038da0a95a69a
Johan Lindell <johan.lindell91@gmail.com>
2023-05-02 13:24:25 +0200

    removed keys

--- app/scripts/main.script.js
+++ app/scripts/main.script.js
@@ -1,8 +1,8 @@
 function fetchWcProducts() {
-  const consumerKey = "ck_46dc8ab29eede8194dcc71ddc4216d70d16dfd34";
-  const consumerSecret = "cs_48f2d8a42a58302528998fa1fd5cb3a960487210";
+  const consumerKey = "";
+  const consumerSecret = "";
   const url =
-    "http://localhost/The%20Silk%20Road%20Project//wp-json/wc/v3/products";
+    "http://localhost/The%20Silk%20Road%20Project/wp-json/wc/v3/products";
 
   const requestOptions = {
     method: "GET",
