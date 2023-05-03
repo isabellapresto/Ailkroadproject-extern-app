f261661ed3ba4592dfb686d3a778ff4103696dd8
Lokacitron123 <104756422+Lokacitron123@users.noreply.github.com>
2023-05-03 14:17:00 +0200

    Update main.script.js

--- app/scripts/main.script.js
+++ app/scripts/main.script.js
@@ -1,21 +1,43 @@
-function fetchWcProducts() {
-  const consumerKey = "";
-  const consumerSecret = "";
-  const url =
-    "http://localhost/The%20Silk%20Road%20Project/wp-json/wc/v3/products";
+document.addEventListener("DOMContentLoaded", function () {
+  function renderData() {
+    const localStorageData = JSON.parse(localStorage.getItem("data"));
+    const container = document.createElement("div");
+    container.classList.add("mainDiv");
 
-  const requestOptions = {
-    method: "GET",
-    headers: new Headers({
-      "Content-Type": "application/json",
-      Authorization: "Basic " + btoa(consumerKey + ":" + consumerSecret),
-    }),
-  };
+    if (localStorageData) {
+      localStorageData.forEach((element) => {
+        console.log(element);
+        const dataDiv = document.createElement("div");
+        const nameHeader = document.createElement("h1");
+        const shortDescription = document.createElement("p");
 
-  fetch(url, requestOptions)
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error));
-}
+        nameHeader.innerHTML = element.name;
+        shortDescription.innerHTML = element.description;
 
-fetchWcProducts();
+        const imgContainer = document.createElement("div");
+        imgContainer.classList.add("imgContainer");
+        dataDiv.appendChild(imgContainer);
+
+        // grab the first image from each product and set its source
+        const firstImage = element.images[0].src;
+        const img = document.createElement("img");
+        img.setAttribute("src", firstImage);
+        imgContainer.appendChild(img);
+
+        dataDiv.appendChild(nameHeader);
+        dataDiv.appendChild(shortDescription);
+        container.appendChild(dataDiv);
+      });
+    } else {
+      container.innerHTML = "No data found in local storage";
+    }
+
+    if (document.body) {
+      document.body.appendChild(container);
+    } else {
+      console.error("Unable to append container element to document body");
+    }
+  }
+
+  renderData();
+});
